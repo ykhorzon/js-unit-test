bd5f3457e8ce1b4cd719a2017307ff5c8aa341fb
YKhorizon <ykhorizon.light@gmail.com>
2021-01-09 23:11:12 +0800

    practice auth

--- src/authentication.js
+++ src/authentication.js
@@ -2,18 +2,28 @@ import {Profile} from "./profile";
 import {Otp} from "./otp";
 
 export class Authentication {
-    is_valid(account, password) {
-        const profile = new Profile();
-        const password_from_profile = profile.get_password(account);
-        const otp = new Otp();
-        const token = otp.get_token();
-        console.log(`password:${password_from_profile}, token:${token}`);
+  is_valid(account, password) {
+    const password_from_profile = this.getPassword(account);
+    const token = this.getToken();
+    console.log(`password:${password_from_profile}, token:${token}`);
 
-        const valid_password = password_from_profile + token;
-        if (valid_password === password) {
-            return true;
-        } else {
-            return false;
-        }
+    const valid_password = password_from_profile + token;
+    if (valid_password === password) {
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  getToken() {
+    const otp = new Otp();
+    const token = otp.get_token();
+    return token;
+  }
+
+  getPassword(account) {
+    const profile = new Profile();
+    const password_from_profile = profile.get_password(account);
+    return password_from_profile;
+  }
 }
--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,8 +1,38 @@
 import {Authentication} from "../src/authentication";
 
 describe('authenticate account is valid', function () {
-    it('should be valid', () => {
-        let authentication = new Authentication();
-        expect(authentication.is_valid('joey', '91000000')).toBe(true);
-    });
+  let authentication = new Authentication();
+  beforeEach(() => {
+    authentication = new Authentication();
+  });
+
+  function givePassword(password) {
+    authentication.getPassword = jest.fn().mockReturnValue(password);
+  }
+
+  function givenToken(token) {
+    authentication.getToken = jest.fn().mockReturnValue(token);
+  }
+
+  function shouldBeValid(account, password) {
+    expect(authentication.is_valid(account, password)).toBe(true);
+  }
+
+  it('should be valid', () => {
+    givePassword('91');
+    givenToken('000000');
+    shouldBeValid('joey', '91000000');
+  });
+
+  function shouldBeInvalid(account, password) {
+    expect(authentication.is_valid(account, password)).toBe(false);
+  }
+
+  it('should be invalid', () => {
+    givePassword('91');
+    givenToken('000000');
+    shouldBeInvalid('joey', 'wrong password');
+  });
+
+
 });
